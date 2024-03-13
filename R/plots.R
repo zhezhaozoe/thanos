269d2a4add0749edec01291aba8bc4a4e077c74e
Zhe Zhao <970816153@qq.com>
2024-03-13 01:07:54 +0100

    make flexible plots

--- R/plots.R
+++ R/plots.R
@@ -1,35 +1,61 @@
-# ps to data table:
-# d <- merge(
-#   melt(
-#     merge(
-#       as.data.table(otu_table(ps))[, ID := rownames(otu_table(ps))],
-#       as.data.table(tax_table(ps)@.Data)[, ID := rownames(tax_table(ps))],
-#       by = "ID"
-#     ),
-#     id.vars = c("ID", colnames(tax_table(ps))),
-#     variable.name = "Sample",
-#     value.name = "Depth"
-#   ),
-#   setattr(as.data.table(sample_data(ps)@.Data), "names", colnames(sample_data(ps)))[, Sample := rownames(sample_data(ps))],
-#   by = "Sample"
-# )
-
-depth_by_sample <- function(out) {
-  out <- merge(out, setattr(as.data.table(sample_data(ps)@.Data), "names", colnames(sample_data(ps)))[, Sample := rownames(sample_data(ps))], by = "Sample")
-  ggplot(out[, .(RelativeDepth = sum(RelativeDepth)), by = c("Sample", "Taxon")], aes(x = Sample, y = RelativeDepth, fill = factor(Sample))) +
-    geom_col()
+# fill by gene for plot 5.
+# this function (as opposed to *_by_sample_group) keeps the sample_data, so it's possible to do + facet_wrap(~whatever) afterwards.
+barplot_depth_by_sample <- function(ps, fill = NULL, position = "stack", ...) {
+  d <- if (is.list(ps)) {
+    rbindlist(lapply(ps_list, function(ps) {
+      setDT(psmelt(ps))
+    }), id = "Gene")
+  } else {
+    setDT(psmelt(ps))
+  }
+  d <- d[, .(Abundance = sum(Abundance)), by = c("Sample", names(sample_data(ps)), fill)]
+  p <- ggplot(d, aes(x = Sample, y = Abundance, fill = if (is.null(fill)) NULL else .data[[fill]])) +
+    geom_col(position = position, ...) +
+    labs(fill = fill) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5))
+  if (all(d[, sum(Abundance), by = Sample]$V1 == 1, na.rm = T) || position == "fill") {
+    p <- p + scale_y_continuous(labels = scales::percent)
+  }
+  p
 }
 
-depth_by_sample_group <- function(out, ps, group = NA) {
-  out <- merge(out, setattr(as.data.table(sample_data(ps)@.Data), "names", colnames(sample_data(ps)))[, Sample := rownames(sample_data(ps))], by = "Sample")
-  ggplot(out2[, .(RelativeDepth = sum(RelativeDepth)), by = .(Group = group, Taxon = Taxon)], aes(x = factor(Group), y = RelativeDepth, fill = Taxon)) +
-    geom_boxplot()
+# barplot_depth_by_group(ps_list, group = "group", fill = "Gene")
+# barplot_depth_by_group(ps_list, group = "group", fill = "Phylum")
+# barplot_depth_by_group(ps_list, group = "Gene", fill = "Phylum")
+# barplot_depth_by_group(ps_list, group = c("Gene", "group"), fill = "Phylum") + facet_wrap(~group, scales = "free")
+# The last one doesn't really work, sadly... maybe adding a wrap argument is a better interface.
+barplot_depth_by_group <- function(ps, group = "Sample", fill = NULL, position = "stack", ...) {
+  d <- if (is.list(ps)) {
+    rbindlist(lapply(ps_list, function(ps) {
+      setDT(psmelt(ps))
+    }), id = "Gene")
+  } else {
+    setDT(psmelt(ps))
+  }
+  d <- d[, .(Abundance = sum(Abundance)), by = c(group, fill)]
+  p <- ggplot(d, aes(x = .data[[group]], y = Abundance, fill = if (is.null(fill)) NULL else .data[[fill]])) +
+    geom_col(position = position, ...) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5)) +
+    labs(fill = fill, x = group) +
+  # The c() in by is necessary, otherwise group will be interpreted as a column of d (if present)
+  if (all(d[, sum(Abundance), by = c(group)]$V1 == 1, na.rm = T) || position == "fill") {
+    p <- p + scale_y_continuous(labels = scales::percent)
+  }
+  p
 }
 
-depth_by_gene <- function(out_list) {
-
-}
-
-percent_depth_by_gene <- function() {
-
+boxplot_depth_by_group <- function(ps, group, x = group, ...) {
+  d <- if (is.list(ps)) {
+    rbindlist(lapply(ps_list, function(ps) {
+      setDT(psmelt(ps))
+    }), id = "Gene")
+  } else {
+    setDT(psmelt(ps))
+  }
+  # The c() in by is necessary, otherwise group will be interpreted as a column of d (if present)
+  d <- d[, .(Abundance = sum(Abundance)), by = c(unique(c(group, x)))]
+  ggplot(d, aes(x = factor(.data[[x]]), y = Abundance)) +
+    geom_boxplot() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5)) +
+    xlab(group)
 }
