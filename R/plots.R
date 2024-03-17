5c164187a3edb97d933825981eece432d218f297
Zhe Zhao <970816153@qq.com>
2024-03-17 20:18:23 +0100

    finalize barplot

--- R/plots.R
+++ R/plots.R
@@ -9,9 +9,13 @@ barplot_depth_by_sample <- function(ps, fill = NULL, position = "stack", ...) {
     setDT(psmelt(ps))
   }
   d <- d[, .(Abundance = sum(Abundance)), by = c("Sample", names(sample_data(ps)), fill)]
-  p <- ggplot(d, aes(x = Sample, y = Abundance, fill = if (is.null(fill)) NULL else .data[[fill]])) +
+  p <- ggplot(d,
+    aes(
+      x = if ("Sample_zzorder" %in% names(sample_data(ps))) Sample_zzorder else Sample,
+      y = Abundance,
+      fill = if (is.null(fill)) NULL else if (paste(fill, "zzorder", sep = "_") %in% names(d)) .data[[paste(fill, "zzorder", sep = "_")]] else .data[[fill]])) +
     geom_col(position = position, ...) +
-    labs(fill = fill) +
+    labs(fill = fill, x = "Sample") +
     theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5))
   if (all(d[, sum(Abundance), by = Sample]$V1 == 1, na.rm = T) || position == "fill") {
     p <- p + scale_y_continuous(labels = scales::percent)
@@ -19,12 +23,11 @@ barplot_depth_by_sample <- function(ps, fill = NULL, position = "stack", ...) {
   p
 }
 
-# barplot_depth_by_group(ps_list, group = "group", fill = "Gene")
-# barplot_depth_by_group(ps_list, group = "group", fill = "Phylum")
-# barplot_depth_by_group(ps_list, group = "Gene", fill = "Phylum")
-# barplot_depth_by_group(ps_list, group = c("Gene", "group"), fill = "Phylum") + facet_wrap(~group, scales = "free")
-# The last one doesn't really work, sadly... maybe adding a wrap argument is a better interface.
-barplot_depth_by_group <- function(ps, group = "Sample", fill = NULL, position = "stack", ...) {
+# barplot_depth(ps_list, group = "group", fill = "Gene")
+# barplot_depth(ps_list, group = "group", fill = "Phylum")
+# barplot_depth(ps_list, group = "Gene", fill = "Phylum")
+# barplot_depth(ps_list, group = c("Gene", "group"), fill = "Phylum") + facet_wrap(~group, scales = "free")
+barplot_depth <- function(ps, group = "Sample", fill = NULL, position = "stack", wrap = NULL, ...) {
   d <- if (is.list(ps)) {
     rbindlist(lapply(ps_list, function(ps) {
       setDT(psmelt(ps))
@@ -32,11 +35,25 @@ barplot_depth_by_group <- function(ps, group = "Sample", fill = NULL, position =
   } else {
     setDT(psmelt(ps))
   }
-  d <- d[, .(Abundance = sum(Abundance)), by = c(group, fill)]
-  p <- ggplot(d, aes(x = .data[[group]], y = Abundance, fill = if (is.null(fill)) NULL else .data[[fill]])) +
-    geom_col(position = position, ...) +
+  by_cols <- c(group, fill, wrap)
+  ordered_by_cols <- c()
+  for (col in by_cols) {
+    ordered_col <- paste(col, "zzorder", sep = "_")
+    if (ordered_col %in% names(d)) {
+      ordered_by_cols <- c(ordered_by_cols, ordered_col)
+    }
+  }
+  d <- d[, .(Abundance = sum(Abundance)), by = c(by_cols, ordered_by_cols)]
+  p <- ggplot(d,
+    aes(
+      x = if (paste(group, "zzorder", sep = "_") %in% names(d)) { .data[[paste(group, "zzorder", sep = "_")]] } else { .data[[group]] },
+      fill = if (is.null(fill)) { NULL } else if (paste(fill, "zzorder", sep = "_") %in% names(d)) { .data[[paste(fill, "zzorder", sep = "_")]] } else { .data[[fill]] },
+      y = Abundance,
+  )) +
+    geom_col(position = position) +
     theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5)) +
     labs(fill = fill, x = group) +
+    if (is.null(wrap)) NULL else facet_wrap(~.data[[wrap]])
   # The c() in by is necessary, otherwise group will be interpreted as a column of d (if present)
   if (all(d[, sum(Abundance), by = c(group)]$V1 == 1, na.rm = T) || position == "fill") {
     p <- p + scale_y_continuous(labels = scales::percent)
@@ -44,7 +61,7 @@ barplot_depth_by_group <- function(ps, group = "Sample", fill = NULL, position =
   p
 }
 
-boxplot_depth_by_group <- function(ps, group, x = group, ...) {
+boxplot_depth <- function(ps, group, x = group, wrap = NULL, ...) {
   d <- if (is.list(ps)) {
     rbindlist(lapply(ps_list, function(ps) {
       setDT(psmelt(ps))
@@ -56,6 +73,7 @@ boxplot_depth_by_group <- function(ps, group, x = group, ...) {
   d <- d[, .(Abundance = sum(Abundance)), by = c(unique(c(group, x)))]
   ggplot(d, aes(x = factor(.data[[x]]), y = Abundance)) +
     geom_boxplot() +
+    if (is.null(wrap)) NULL else facet_wrap(~.data[[wrap]]) +
     theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5)) +
     xlab(group)
 }
