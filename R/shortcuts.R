169e22bee1c1bf06e5b35210917bb5c16d9f0dda
Zhe Zhao <970816153@qq.com>
2024-03-22 21:05:39 +0100

    add shortcuts

---
+++ R/shortcuts.R
@@ -0,0 +1,37 @@
+get_contigs_hits_depths <- function(depths_files, pattern, replacement, query_tblout, control_tblout, linker = contigs_linker, verbose = F) {
+  # We use seq_along here because iterating over the depth_files directly
+  # the name attribute is lost, and the name is used in import_contig_depths
+  mers <- lapply(seq_along(depths_files), function(i) {
+    cdf <- depths_files[i]
+    if (isTRUE(verbose)) {
+      message(cdf)
+    }
+    otus <- import_contig_depths(cdf, pattern, replacement)
+    get_hits_depths(otus, query_tblout, control_tblout, linker, phyloseq = F)
+  })
+  mer <- rbindlist(mers, use.names = T)[, lapply(.SD, sum), by = "ID"]
+  res <- mer[, .SD, .SDcols = patterns("*\\.x$")] / mer[, .SD, .SDcols = patterns("*\\.y$")]
+  setnames(res, names(res), sub("\\.x$", "", names(res)))
+  m <- as.matrix(res, rownames.value = mer$ID)
+  # XXX: set NaNs (0/0) to zero
+  m[is.nan(m)] <- 0
+  otu_table(m, taxa_are_rows = T)
+}
+
+get_hits_depths_from_ko <- function(kos, ps, dbs, control_tblout, linker, msa_method = "Muscle", cpu = 1, incE = 1e-6, taxrank = NULL) {
+  lapply(kos, function(ko) {
+    aln <- get_kegg_msa(ko, method = msa_method)
+    hmm <- build_hmm(aln)
+    query_tblout <- search_hmm(hmm, dbs, cpu = cpu, incE = incE)
+    get_hits_depths(ps, query_tblout, control_tblout, linker, taxrank = taxrank)
+  })
+}
+
+get_contigs_hits_depths_from_ko <- function(kos, depths_files, pattern, replacement, dbs, control_tblout, linker = contigs_linker, msa_method = "Muscle", cpu = 1, incE = 1e-6, verbose = F) {
+  lapply(kos, function(ko) {
+    aln <- get_kegg_msa(ko, method = msa_method)
+    hmm <- build_hmm(aln)
+    query_tblout <- search_hmm(hmm, dbs, cpu = cpu, incE = incE)
+    get_contigs_hits_depths(depths_files, pattern, replacement, query_tblout, control_tblout, linker = linker, verbose = verbose)
+  })
+}
