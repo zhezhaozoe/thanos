2441f76b8154e34af3b7801236117094bf70bc9a
Zhe Zhao <970816153@qq.com>
2024-03-24 10:28:57 +0100

    document ordering and shortcuts

--- R/ordering.R
+++ R/ordering.R
@@ -1,8 +1,44 @@
+#' Order samples in phyloseq object
+#'
+#' Reorders the samples in a `phyloseq` object based on the specified metadata column.
+#'
+#' This function reorders the samples in a given `phyloseq` object according to the
+#' values of a specified metadata column in ascending order. This can be useful for
+#' plotting or analyses where the order of samples based on metadata is relevant.
+#'
+#' @param ps A `phyloseq` object containing sample data.
+#' @param by The name of the column in the sample metadata to order the samples by.
+#'
+#' @return A `phyloseq` object with reordered samples.
+#'
+#' @examples
+#' # Assuming `ps` is a phyloseq object and `pH` is a metadata column
+#' ps_ordered <- order_samples_by(ps, "pH")
+#'
+#' @export
 order_samples_by <- function(ps, by) {
   levels <- rownames(sample_data(ps))[order(sample_data(ps)[[by]])]
   set_group_order(ps, "Sample", levels)
 }
 
+#' Set custom order for groups in phyloseq object
+#'
+#' This function sets a custom ordering for a specified group within the sample data of a phyloseq object. The new ordering is stored in a new column within the sample data, distinguishing between a special case for the "Sample" group and other groups.
+#'
+#' @param ps A phyloseq object containing sample data.
+#' @param group The name of the group (column in the sample data) to set the order for. If the group is "Sample", the function treats it as a special case.
+#' @param levels A vector of values defining the new order of the group.
+#'
+#' @return A modified phyloseq object with the new order set for the specified group.
+#'
+#' @examples
+#' # Given a phyloseq object `ps`, set a new order for the "Sample" group
+#' ps_new <- set_group_order(ps, "Sample", c("Sample3", "Sample1", "Sample2"))
+#'
+#' # For a group "Treatment", set a new order
+#' ps_new <- set_group_order(ps, "Treatment", c("Control", "Treatment1", "Treatment2"))
+#'
+#' @export
 set_group_order <- function(ps, group, levels) {
   if (group == "Sample") {
     sample_data(ps)$Sample_zzorder = factor(rownames(sample_data(ps)), levels = levels, ordered = T)
--- R/shortcuts.R
+++ R/shortcuts.R
@@ -1,9 +1,41 @@
-tblout_from_ko <- function(ko, method = "Muscle", cpu = 1, incE = 1e-6) {
-  aln <- get_kegg_msa(ko, method = method)
+#' Perform HMM Search on a KEGG Orthology (KO) Alignment
+#'
+#' Given a KO identifier, this function retrieves the corresponding protein sequences and builds a Multiple Sequence Alignment (MSA), then builds a Hidden Markov Model (HMM) based on the MSA, and then searches
+#' the HMM against a set of sequence databases. The function allows adjusting the search method,
+#' computational resources, and inclusion threshold.
+#'
+#' @param ko A character string representing the KEGG Orthology (KO) identifier.
+#' @param method Character. The method used for performing the MSA. Defaults to "Muscle".
+#' @param cpu Integer. The number of CPUs to use for the search. Defaults to 1.
+#' @param incE Numeric. The inclusion threshold for the HMM search. Defaults to 1e-6.
+#' @param ... Additional arguments passed to kegg_kegg_msa().
+#'
+#' @return The result of the HMM search.
+#' @examples
+#' tblout_from_ko("K00001")
+#' @export
+tblout_from_ko <- function(ko, method = "Muscle", cpu = 1, incE = 1e-6, ...) {
+  aln <- get_kegg_msa(ko, method = method, ...)
   hmm <- build_hmm(aln)
   search_hmm(hmm, dbs, cpu = cpu, incE = incE)
 }
 
+#' Obtain Contigs Hits Depths from Depth Files
+#'
+#' This function processes depth files to extract and calculate the hits depths for contigs.
+#' Contig depths files are typically large (several hundred MB per sample), therefore, with many samples, it's impractical to load all of the data at once in memory. This function provides a way to calculate the depths sample-wise, to avoid storing all the data in memory.
+#'
+#' @param depths_files A character vector of file paths to the depths files.
+#' @param pattern Character. A regular expression pattern to match in contigs names within the depth files.
+#' @param replacement Character. A string to replace the matched pattern in contig names.
+#' @param query_tblout File path to the tblout file containing query hits.
+#' @param control_tblout File path to the tblout file containing control hits.
+#' @param linker A function to link contig identifiers between the query and control tblout files.
+#' @param verbose Logical. Indicates whether to print detailed processing messages. Defaults to FALSE.
+#' @return A phyloseq object with the relative depths of query vs control genes across all `detphs_files`.
+#' @examples
+#' get_contigs_hits_depths(c("depths1.txt", "depths2.txt"), "_contig", "_sequence", "query.tblout", "control.tblout")
+#' @export
 get_contigs_hits_depths <- function(depths_files, pattern, replacement, query_tblout, control_tblout, linker = contigs_linker, verbose = F) {
   # We use seq_along here because iterating over the depth_files directly
   # the name attribute is lost, and the name is used in import_contig_depths
@@ -24,6 +56,34 @@ get_contigs_hits_depths <- function(depths_files, pattern, replacement, query_tb
   otu_table(m, taxa_are_rows = T)
 }
 
+#' Retrieve depths of hits from KEGG Orthology entries across samples
+#'
+#' This function takes KEGG Orthology (KO) identifiers, a phyloseq object, a set of databases, 
+#' a control tblout formatted file, and computes 
+#' the depths of hits related to the input KOs in the provided sample set.
+#'
+#' @param kos A character vector of KEGG Orthology (KO) identifiers.
+#' @param ps A phyloseq object containing the samples data.
+#' @param dbs A character vector indicating the databases to be used.
+#' @param control_tblout A tblout formatted file with the control genes' hits.
+#' @param linker Function that links OTU identifiers between tblout tables and the phyloseq object. The function must take two arguments: SeqFile and Target.
+#' @param msa_method The method used for multiple sequence alignment. "Muscle" by default.
+#' @param cpu The number of CPUs to be used for computation. Integer value. 1 by default.
+#' @param incE The inclusion E-value threshold for considering a hit significant. Numeric value. 1e-6 by default.
+#' @param taxrank Optional; character specifying the taxonomic rank at which to aggregate hits. Valid options depend on the taxonomic ranks present in the `ps` object. If not specified, no aggregation is performed.
+#'
+#' @return A list where each element corresponds to a KO identifier inputted and contains
+#'         the computed depths of hits for that KO in the sample set.
+#'
+#' @examples
+#' kos <- c("K00001", "K00002")
+#' ps <- phyloseq_object # a pre-loaded or created phyloseq object
+#' dbs <- c("db1", "db2")
+#' control_tblout <- "control_hits.tblout"
+#' linker <- read.csv("linker_table.csv")
+#' hit_depths <- get_hits_depths_from_kos(kos, ps, dbs, control_tblout, linker)
+#' 
+#' @export
 get_hits_depths_from_kos <- function(kos, ps, dbs, control_tblout, linker, msa_method = "Muscle", cpu = 1, incE = 1e-6, taxrank = NULL) {
   lapply(kos, function(ko) {
     query_tblout <- tblout_from_ko(ko, method = msa_method, cpu = cpu, incE = incE)
@@ -31,6 +91,37 @@ get_hits_depths_from_kos <- function(kos, ps, dbs, control_tblout, linker, msa_m
   })
 }
 
+#' Get Contigs Hits Depths from KEGG Orthology (KO) Identifiers
+#'
+#' For each KEGG Orthology (KO) identifier provided, this function retrieves contig hits and their depths from the
+#' specified depths files.
+#'
+#' @param kos A character vector of KEGG Orthology (KO) identifiers.
+#' @param depths_files A character vector indicating the paths to depths files.
+#' @param pattern A regex pattern used to match and extract contig names from the depths files.
+#' @param replacement A character string to replace in the extracted contig names, based on the `pattern` argument.
+#' @param dbs A not used parameter in the current implementation.
+#' @param control_tblout Character string indicating the path to the tblout file from the control.
+#' @param linker A function that links contig names to their hits and depths. Defaults to `contigs_linker`.
+#' @param msa_method Character string representing the method used for multiple sequence alignment. Defaults to `"Muscle"`.
+#' @param cpu An integer indicating the number of CPUs to be used in computations. Defaults to `1`.
+#' @param incE A numeric value specifying the inclusion E-value threshold. Defaults to `1e-6`.
+#' @param verbose A logical indicating whether to display detailed output. Defaults to `FALSE`.
+#'
+#' @return Returns a list where each element corresponds to the result from a single KO identifier, containing the contig hits and their depths information.
+#'
+#' @examples
+#' \dontrun{
+#' kos <- c("K00001", "K00002")
+#' depths_files <- c("sample1.depths", "sample2.depths")
+#' pattern <- "_(\d+)$"
+#' replacement <- ""
+#' control_tblout <- "control.tblout"
+#' results <- get_contigs_hits_depths_from_kos(kos, depths_files, pattern, replacement, NULL, control_tblout)
+#' }
+#'
+#' @importFrom lapply
+#' @export
 get_contigs_hits_depths_from_kos <- function(kos, depths_files, pattern, replacement, dbs, control_tblout, linker = contigs_linker, msa_method = "Muscle", cpu = 1, incE = 1e-6, verbose = F) {
   lapply(kos, function(ko) {
     query_tblout <- tblout_from_ko(ko, method = msa_method, cpu = cpu, incE = incE)
